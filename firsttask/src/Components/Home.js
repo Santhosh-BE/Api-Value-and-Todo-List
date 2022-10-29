42a043af4f6c889409c25436f7b015421e81123c
santhosh <santhoshkaliannann@gmail.com>
2022-10-29 13:33:39 +0530

    Adding Style for Pagination in Home Page

--- firsttask/src/Components/Home.js
+++ firsttask/src/Components/Home.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import  ReactPaginate from 'react-paginate'
 import { Link, useNavigate } from "react-router-dom"
+import './Home.css'
 
 
 function Home() {
